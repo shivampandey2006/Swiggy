c24252f5ea0cb75b0604bc5fec21b40cd63caa37
Shivam Pandey <shivampandey@111888l.com>
2026-08-07 12:39:14 +0530

    add

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,24 +1,37 @@
 
+import { useState } from "react";
 import RestaurantsData from "../asset/RestaurentsData"
 import Footer from "./Footer";
 
+const Rest =
+  RestaurantsData[0].data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
+
 export default  function Restaurants(){
 
+  const [Cafe , SetCafe] = useState(Rest)
+  
+function SortedByCost() {
+  const SortedCost = [...Cafe].sort((a, b) => {
+    const costA = parseInt(a.info.costForTwo.replace(/\D/g, ""));
+    const costB = parseInt(b.info.costForTwo.replace(/\D/g, ""));
+    return costA - costB;
+  });
 
-const Restaurants =
-  RestaurantsData[0].data.cards[2].card.card.gridElements.infoWithStyle.restaurants;
-console.log(Restaurants.length);
+  SetCafe(SortedCost);
+}
+
   return (
     <>
    
       <div className="" >
+    <div>  <button className="ml-10 mt-20 bg-orange-600 p-2 rounded-2xl text-white text-[20px] cursor-pointer hover:bg-orange-500  " onClick={SortedByCost}>Sort By Price</button></div>  
       <div className="ml-[30] mt-[40] mb-[40] text-2xl  font-bold  ">Order From Your Fav One</div>
-      <div className="flex gap-10 flex-wrap w-[80%] mx-auto items-center justify-center mb-30 " >
+      <div className="flex gap-10 flex-wrap w-[80%] mx-auto items-start justify-center mb-30 " >
       {
-        Restaurants.map((value)=>(
+        Cafe.map((value)=>(
           <div key={value.info.id}className="  w-70   " >
-           <div className="h-50 w-70  " >
-             <img className="h-full w-full  rounded-2xl "
+           <div className="h-50   " >
+             <img className="object-cover h-full w-full  rounded-2xl cursor-pointer transition-transform duration-300 hover:scale-105  "
               src={`https://media-assets.swiggy.com/swiggy/image/upload/${value.info.cloudinaryImageId}`} />
             </div>
 
